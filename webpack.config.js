dd0cf5d1d16267158df3e1a8dcf4d527e463e621
mowslasher83 <radeswaled@hotmail.com>
2025-08-28 20:46:09 -0500

    Add comments to clarify webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const path = require('path');
 
 module.exports = {
+  // Entry point of the application
   entry: './src/index.js',
   output: {
+    // Output directory and filename
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        // Rule for JavaScript and JSX files
+        test: /\.jsx?$/, 
         exclude: /node_modules/, 
         use: {
           loader: 'babel-loader',
@@ -19,15 +22,18 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
+        // Rule for CSS files
+        test: /\.css$/, 
         use: ['style-loader', 'css-loader']
       }
     ]
   },
   resolve: {
+    // File extensions that should be resolved
     extensions: ['.js', '.jsx']
   },
   devServer: {
+    // Static content served from the 'dist' directory
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000
